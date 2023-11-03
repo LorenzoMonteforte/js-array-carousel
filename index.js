810dd157cc3b84db391d2dbd82ebeca09470c3bf
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-03 02:35:30 +0100

    Aggiunta commenti JS e modifica let/const

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
-let percorsiImg = ["consegna/consegna/img/01.webp", "consegna/consegna/img/02.webp", "consegna/consegna/img/03.webp", "consegna/consegna/img/04.webp", "consegna/consegna/img/05.webp"];
-let fotoContainer = document.getElementById("fotoContainer");
-let immagini = [];
+// Percorsi immagine
+const percorsiImg = ["consegna/consegna/img/01.webp", "consegna/consegna/img/02.webp", "consegna/consegna/img/03.webp", "consegna/consegna/img/04.webp", "consegna/consegna/img/05.webp"];
+// Foto principale
+const fotoContainer = document.getElementById("fotoContainer");
+const immagini = [];
 for(let i=0; i<percorsiImg.length; i++){
-    let img = document.createElement("img");
+    const img = document.createElement("img");
     img.src = percorsiImg[i];
     if(!(i==0)){
         img.classList.add("disNon");
@@ -10,10 +12,11 @@ for(let i=0; i<percorsiImg.length; i++){
     immagini.push(img);
     fotoContainer.appendChild(img);
 }
-let miniFotoContainer = document.getElementById("miniFotoContainer");
-let miniImmagini = [];
+// Mini foto
+const miniFotoContainer = document.getElementById("miniFotoContainer");
+const miniImmagini = [];
 for(let i=0; i<percorsiImg.length; i++){
-    let img = document.createElement("img");
+    const img = document.createElement("img");
     img.src = percorsiImg[i];
     if(!(i==0)){
         img.style.opacity = 0.4;
@@ -21,8 +24,10 @@ for(let i=0; i<percorsiImg.length; i++){
     miniImmagini.push(img);
     miniFotoContainer.appendChild(img);
 }
+// Index
 let index = 0;
-let avanti = document.getElementById("avanti");
+// Btn avanti
+const avanti = document.getElementById("avanti");
 avanti.addEventListener("click", function(){
     if(index==(immagini.length-1)){
         immagini[index].classList.add("disNon");
@@ -38,7 +43,8 @@ avanti.addEventListener("click", function(){
         miniImmagini[index].style.opacity = 1;
     }
 })
-let indietro = document.getElementById("indietro");
+// Btn indietro
+const indietro = document.getElementById("indietro");
 indietro.addEventListener("click", function(){
     if(index==0){
         immagini[index].classList.add("disNon");
