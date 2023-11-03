69fb080b7d805eee4d32e3b37a28f417ca84257a
Lorenzo Monteforte <lorenzomonteforte.mm@mail.com>
2023-11-03 02:30:14 +0100

    Bonus 2

--- index.js
+++ index.js
@@ -10,28 +10,47 @@ for(let i=0; i<percorsiImg.length; i++){
     immagini.push(img);
     fotoContainer.appendChild(img);
 }
+let miniFotoContainer = document.getElementById("miniFotoContainer");
+let miniImmagini = [];
+for(let i=0; i<percorsiImg.length; i++){
+    let img = document.createElement("img");
+    img.src = percorsiImg[i];
+    if(!(i==0)){
+        img.style.opacity = 0.4;
+    }
+    miniImmagini.push(img);
+    miniFotoContainer.appendChild(img);
+}
 let index = 0;
 let avanti = document.getElementById("avanti");
 avanti.addEventListener("click", function(){
     if(index==(immagini.length-1)){
         immagini[index].classList.add("disNon");
+        miniImmagini[index].style.opacity = 0.4;
         index = 0;
         immagini[index].classList.remove("disNon");
+        miniImmagini[index].style.opacity = 1;
     }else{
         immagini[index].classList.add("disNon");
+        miniImmagini[index].style.opacity = 0.4;
         index++;
         immagini[index].classList.remove("disNon");
+        miniImmagini[index].style.opacity = 1;
     }
 })
 let indietro = document.getElementById("indietro");
 indietro.addEventListener("click", function(){
     if(index==0){
         immagini[index].classList.add("disNon");
+        miniImmagini[index].style.opacity = 0.4;
         index = (immagini.length-1);
         immagini[index].classList.remove("disNon");
+        miniImmagini[index].style.opacity = 1;
     }else{
         immagini[index].classList.add("disNon");
+        miniImmagini[index].style.opacity = 0.4;
         index--;
         immagini[index].classList.remove("disNon");
+        miniImmagini[index].style.opacity = 1;
     }
 })
